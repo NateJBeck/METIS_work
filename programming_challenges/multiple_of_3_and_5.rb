f82b7d6a51300c5f7fa8e9ac5493e41a1a156523
Nathan Beck <nathan.beck10@gmail.com>
2014-09-18 09:58:41 -0400

    completed

--- programming_challenges/multiple_of_3_and_5.rb
+++ programming_challenges/multiple_of_3_and_5.rb
@@ -1,24 +1,18 @@
 class MultipleSum
 	def initialize
 		@numbers = (1..10)
-		@numbers_divisible_by_3_or_5 = []	
+		@numbers_divisible_by_3_or_5 = []
+		@sum = 0	
 	end
 
 	def play
-		put_numbers_div_by_3_or_5_into_array
-		puts @numbers_divisible_by_3_or_5
+		get_numbers_div_by_3_or_5_into_array
+		add_the_numbers_together
+		puts @sum
 
-		sum = METHOD SUMMING EVERYTHING
-
-		print sum
-		print_sum_of_the_numbers_in_array
-
-		#go through all the numbers
-		#find numbers div by 3
-		#put into numbers_divisible_by_3_or_5
 	end
 
-	def put_numbers_div_by_3_or_5_into_array
+	def get_numbers_div_by_3_or_5_into_array
 		@numbers.each do |num|
 			if num % 3 == 0 || num % 5 == 0
 				@numbers_divisible_by_3_or_5 << num
@@ -27,7 +21,10 @@ class MultipleSum
 		@numbers_divisible_by_3_or_5.pop
 	end
 
-	def print_sum_of_the_numbers_in_array
+	def add_the_numbers_together
+		@numbers_divisible_by_3_or_5.each do |num|
+			@sum += num
+		end
 	end
 
 end
