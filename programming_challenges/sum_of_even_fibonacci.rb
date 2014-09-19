ee5329c053247c46002dd60e0f1042c10860c5e6
Nathan Beck <nathan.beck10@gmail.com>
2014-09-19 13:43:12 -0400

    Fibonacci sequencing in this might not be awesome but it works

--- programming_challenges/sum_of_even_fibonacci.rb
+++ programming_challenges/sum_of_even_fibonacci.rb
@@ -1,29 +1,61 @@
 class Fibonacci
-	def initialize(highest)
-		@highest = highest
-		@all_numbers = []
-		@fibonacci_values = []
-
-		@highest-1.times do |num|
-			@all_numbers << num
-		end
-
+	def initialize(number)
+		@max_number = number
+		
+		@fibonacci_sequence = []
+		@even_fibonacci_values = []
 	end
 
 	def play
-		fibonaccify_the_numbers
+		make_fibonacci_sequence_less_than_number
+		make_array_of_even_fibonacci
+		get_total_of_all_even_values_and_print_it
 	end
 
-	def fibonaccify_the_numbers
-		@.times.map do |num|
-			@fibonacci_values << (@all_numbers[num] + @all_numbers[num+1])			
+	def make_fibonacci_sequence_less_than_number
+		first = 1
+		second = 1
+		total = 1
+
+		1000.times do |num|
+
+			if num <= 1
+				total = first + num
+			else
+				total = first + second
+			end
+
+			second = first
+			first = total
+
+			if total >= @max_number
+				break
+			end			
+			
+			@fibonacci_sequence << total
+
 		end
 	end
+
+	def make_array_of_even_fibonacci
+		  @fibonacci_sequence.each do |value|
+			if value % 2 == 0
+				@even_fibonacci_values << value
+			end
+		end
+	end
+
+	def get_total_of_all_even_values_and_print_it
+		sum = 0
+
+		@even_fibonacci_values.each do |value|
+			sum += value	
+		end
+
+		puts "Total of all even fibonacci values less than #{@max_number}: #{sum}"
+	end
+
 end
 
-fibonacci = Fibonacci.new(40)
+fibonacci = Fibonacci.new(50000000000)
 fibonacci.play
-
-
-#.map collects output
-#3.times.map {  }
